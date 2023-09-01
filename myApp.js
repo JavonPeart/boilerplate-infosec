47244538721b809b2da156a1792960a5f8e9e66f
JavonPeart <44687372+JavonPeart@users.noreply.github.com>
2023-09-01 13:26:17 -0500

    Render definition

--- myApp.js
+++ myApp.js
@@ -1,63 +1,10 @@
-const express = require('express');
+const express = require("express");
 const helmet = require('helmet');
 const app = express();
+const PORT = process.env.PORT || 3030;
 
+// your code
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`👨‍💻 FCC InfoSec Project by NaFeuer started on port ${PORT}`);
 });
